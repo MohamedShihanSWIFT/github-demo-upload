b8869db90e18c34d30777ab2e20b9c99089990d9
Shihan <shihankarikal2011@gmail.com>
2023-06-24 13:52:04 +0400

    Initial Commit

---
+++ GithubDemoUpload/GithubDemoUploadApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubDemoUploadApp.swift
+//  GithubDemoUpload
+//
+//  Created by Mohamed Shihan on 24/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct GithubDemoUploadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
